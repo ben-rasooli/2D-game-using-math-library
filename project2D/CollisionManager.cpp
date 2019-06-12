75b12de84d0c0cd34d9bf903c4a3859e9f811cc0
ben-rasooli <ben.rasooli@gmail.com>
2019-06-12 16:11:25 +1000

    Clean up and add comment for CollisionManager

--- project2D/CollisionManager.cpp
+++ project2D/CollisionManager.cpp
@@ -34,28 +34,27 @@ CollisionManager * CollisionManager::I()
 	return _inst;
 }
 
+// Each ColliderSet contains a layer property and the two following collections,
+// ColliderLayers: tells which other ColliderSets can this ColliderSet possibly collide with,
+// ColliderOwners: tells what GameObjects belong to this ColliderSet.
+//
+// In every frame, it goes through each ColliderSet 
+// and finds all its potential collidable GameObjects/Colliders from other collidable layers
+// and check against them to detect a possible collision.
+// Then, in case of collision it notify the collided GameObjects.
 void CollisionManager::Update(float deltaTime)
 {
 	for (int i = 0; i < _colliderSets.Count(); i++)
-	{
-		// find target collider owners inside collidableLayers
 		for (int ii = 0; ii < _colliderSets[i]->CollidableLayers.Count(); ii++)
-		{
 			for (int iii = 0; iii < _colliderSets.Count(); iii++)
 				if (_colliderSets[iii]->Layer == _colliderSets[i]->CollidableLayers[ii])
-				{
 					for (int iiii = 0; iiii < _colliderSets[i]->ColliderOwners.Count(); iiii++)
 						for (int iiiii = 0; iiiii < _colliderSets[iii]->ColliderOwners.Count(); iiiii++)
 						{
 							GameObject* targetColliderOwner = _colliderSets[iii]->ColliderOwners[iiiii];
 							if (_colliderSets[i]->ColliderOwners[iiii]->GetCollider()->IsCollidingWith(targetColliderOwner->GetCollider()))
-							{
 								targetColliderOwner->OnCollision(_colliderSets[i]->ColliderOwners[iiii]);
-							}
 						}
-				}
-		}
-	}
 }
 
 void CollisionManager::Draw(aie::Renderer2D* renderer)
